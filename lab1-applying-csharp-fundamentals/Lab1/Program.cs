3c8268ce2db9256cac33deb48532b96f4f94ea6b
abigailgd <184506684+abigailgd@users.noreply.github.com>
2025-09-30 09:13:03 +0100

    Lab Sheet 01: Applying C# Fundamentals

---
+++ lab1-applying-csharp-fundamentals/Lab1/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Task 2: Storing and Displaying Personal Data
+        /* string name = "Abbie";
+        int num = 7;
+        string hometown = "Leicester";
+        Console.WriteLine(name);
+        Console.WriteLine(num);
+        Console.WriteLine(hometown); */
+
+        // Task 3: Crafting a Profile with String Interpolation
+        /* int age = 18;
+        string name = "Abbie";
+        Console.WriteLine($"User Profile: {name} (Age: {age})"); */
+
+        // Task 4: A Story of a Bank Account
+        /* int balance = 1000;
+        Console.WriteLine($"Initial balance: {balance}");
+        balance -= 150;
+        Console.WriteLine($"Balance after withdrawal: {balance}");
+        balance += 375;
+        Console.WriteLine($"Final balance after deposit: {balance}"); */
+
+        // Task 5: The Classic Swap Puzzle
+        /* string bottleA = "Water";
+        string bottleB = "Juice";
+        Console.WriteLine($"Before swap: bottleA contains {bottleA}, bottleB contains {bottleB}");
+        string x = bottleA;
+        bottleA = bottleB;
+        bottleB = x;
+        Console.WriteLine($"After swap: bottleA contains {bottleA}, bottleB contains {bottleB}"); */
+
+        // Task 6: The Bug Hunt
+        // This code has errors!
+        /* int score = 1500;
+        string username = "Player1";
+
+        Console.WriteLine($"User: {username} has reached score {score}"); */
+    }
+}
